7bf6db5a1b105e2fa0c7f873b14cc45570410d59
otavioboyko <100617725+otavioboyko@users.noreply.github.com>
2022-04-03 14:44:12 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ aula05/ex001.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`a velocidade de seu carro e de ${vel}km/h`)
+if (vel > 60) { //condiçao simples
+    console.log(`voce ultrapassou o limite de velocidade, voce esta MULTADO`)
+}
+console.log(`use sempre cinto de segurança`)
---
+++ aula05/ex002.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`vivendo em ${pais}`)
+if (pais == 'brasil'){
+    console.log('voce e brasileiro')
+} else {
+    console.log('voce e estrangeiro')
+}
