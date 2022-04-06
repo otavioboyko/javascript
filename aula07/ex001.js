5b253de671f3db49956c9ecb033c4a1733f33bc4
otavioboyko <100617725+otavioboyko@users.noreply.github.com>
2022-04-06 20:34:41 -0300

    b

---
+++ aula07/ex001.js
@@ -0,0 +1,16 @@
+var c = 1
+while (c < 6) {
+    console.log('tudo bem?')
+    c++ // c = c + 1
+}
+
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
