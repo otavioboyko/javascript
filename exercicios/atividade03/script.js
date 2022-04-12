9c9fdf9849dcae5b74d28b75e06103d0b1ccc2cf
otavioboyko <100617725+otavioboyko@users.noreply.github.com>
2022-04-12 15:24:31 -0300

    cgchchhj

---
+++ exercicios/atividade03/script.js
@@ -0,0 +1,9 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if {
+        var fsex = Document.get
+    }
+}
