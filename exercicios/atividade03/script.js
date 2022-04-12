68021e7f99f33224a58702918fe93bc10e45cf77
otavioboyko <100617725+otavioboyko@users.noreply.github.com>
2022-04-12 19:05:56 -0300

    bvjbjk

--- exercicios/atividade03/script.js
+++ exercicios/atividade03/script.js
@@ -1,6 +1,13 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fAno = document.getElementById('txtano')
+    var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
+    if (fano.ariaValueMin.length == 0 || Number(fano.ariaValueMin) > ano) {
+        window.alert('ERRO!!! verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `idade calculada ${idade}`
+    }
 }
