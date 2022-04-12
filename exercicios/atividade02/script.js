845649bde489960c1f3ef0fa666131534a4784f3
otavioboyko <100617725+otavioboyko@users.noreply.github.com>
2022-04-12 14:23:52 -0300

    h

--- exercicios/atividade02/script.js
+++ exercicios/atividade02/script.js
@@ -3,7 +3,7 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 9
+    //var hora = 15
     msg.innerHTML = `agora sao ${hora} horas`
     if (hora >= 0 &&  hora < 12) {
         //bom dia
