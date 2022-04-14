1d1b2833d4bd91c06b7ba2b004eed0f8df7046e3
otavioboyko <100617725+otavioboyko@users.noreply.github.com>
2022-04-14 15:49:44 -0300

    khjbbhkjhbkj

--- exercicios/atividade04/script.js
+++ exercicios/atividade04/script.js
@@ -1,1 +1,12 @@
-alert('ola')
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res =
+
+    if (ini.ariaValueMax.length == 0 || passo.ariaValueNow.length == 0) {
+        window.alert('ERRO!!! numero invalido')
+    } else {
+        window.alert('tudo ok')
+    }
+}
