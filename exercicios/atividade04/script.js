35e5e2a7959e4b8de041f5d7566e19917b834e5b
otavioboyko <100617725+otavioboyko@users.noreply.github.com>
2022-04-14 15:55:04 -0300

    jkk

--- exercicios/atividade04/script.js
+++ exercicios/atividade04/script.js
@@ -2,11 +2,11 @@ function contar() {
     var ini = document.getElementById('txti')
     var fim = document.getElementById('txtf')
     var passo = document.getElementById('txtp')
-    var res =
+    var res = document.getElementById('res')
 
-    if (ini.ariaValueMax.length == 0 || passo.ariaValueNow.length == 0) {
+    if (ini.nodeValue.length == 0 || passo.nodeValue.length == 0) {
         window.alert('ERRO!!! numero invalido')
     } else {
-        window.alert('tudo ok')
+       res.innerHTML = 'contando:'
     }
 }
