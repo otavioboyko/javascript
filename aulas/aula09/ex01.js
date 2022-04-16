bf06e2ce9e5f4507786695038fe90d920edd817e
otavioboyko <100617725+otavioboyko@users.noreply.github.com>
2022-04-16 13:17:52 -0300

    mvbjhg

---
+++ aulas/aula09/ex01.js
@@ -0,0 +1,10 @@
+let num = [15, 20, 1, 22, 9, 15]
+num.push(1)
+/*num.sort()
+console.log(num)
+console.log(`o vetor tem ${num.length} posiçoes`)
+console.log(num[0])
+console.log(`o primeiro valor do vetor e ${num[0]}`)*/
+
+let pos = num.indexOf(20)
+console.log(`o valor 20 esta na posiçao ${pos}`)
