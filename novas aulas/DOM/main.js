f7ba480249c88671334b819e57c4e0f848e9310e
otavioboyko <100617725+otavioboyko@users.noreply.github.com>
2022-05-13 19:59:23 -0300

    lkhliuhlkhhlh

---
+++ novas aulas/DOM/main.js
@@ -0,0 +1,9 @@
+//selecionar apenas um elemento
+//const addUsertext = document.getElementById('add-user')
+//const myform = document.querySelector(".item")
+
+//selecionar multiplos elementos
+//const todos = document.querySelectorAll(".item")
+//const todos = document.getElementsByClassName('item')
+const todos = document.getElementsByTagName('li')
+console.log(todos)
