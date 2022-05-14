17525d454de0c7199e8bbf16457fc3eb42f6fa5d
otavioboyko <100617725+otavioboyko@users.noreply.github.com>
2022-05-14 13:06:22 -0300

    vjhvjhvjhvjkh

--- novas aulas/DOM/main.js
+++ novas aulas/DOM/main.js
@@ -5,5 +5,16 @@
 //selecionar multiplos elementos
 //const todos = document.querySelectorAll(".item")
 //const todos = document.getElementsByClassName('item')
-const todos = document.getElementsByTagName('li')
-console.log(todos)
+//const todos = document.getElementsByTagName('li')
+
+const item = document.querySelector(".items")
+const botao = document.querySelectorAll('.btn')
+
+//item.remove() remove todos os itens
+//item.firstElementChild.remove() //firstElementChild pega o primeiro filho da coisa que voce quer. E o remove remove o primeiro filho
+//item.lastElementChild.remove()//lastElementChild e a mesma coisa que o anterior so que ele pega o ultimo
+//item.children[1].textContent = "item dois" //o children e uma lista
+//item.lastElementChild.innerHTML = "<h1>oi</h1>" //podemos modificar o html com o inner html
+
+botao.style.background = "red"; //muda a cor do botao
+botao.style.color = "blue" //muda coisas que estao no botao
